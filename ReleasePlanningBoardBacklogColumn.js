5af252494e09b2a564832d767f3ca883b4b06583
Colin O'Brien <cobrien@rallydev.com>
2013-11-25 21:55:49 -0500

    Initial Commit

---
+++ ReleasePlanningBoardBacklogColumn.js
@@ -0,0 +1,135 @@
+(function() {
+    var Ext = window.Ext4 || window.Ext;
+
+    /**
+     * @private
+     * Should this be in rui?
+     */
+    Ext.define('Rally.apps.releaseplanningboard.ReleasePlanningBoardBacklogColumn', {
+        extend: 'Rally.ui.cardboard.Column',
+        alias: 'widget.releaseplanningboardappbacklogcolumn',
+
+        cls: 'column',
+
+        requires: [
+            'Rally.ui.TextField'
+        ],
+
+        mixins: {
+            maskable: 'Rally.ui.mask.Maskable'
+        },
+
+        config: {
+            value: null
+        },
+
+        getColumnStatus: function() {
+            return this.columnStatus;
+        },
+
+        getStatusCell: function() {
+            return Ext.get(this.statusCell);
+        },
+
+        drawHeader: function() {
+            this.callParent(arguments);
+
+            this.getColumnHeader().add(
+                {
+                    xtype: 'container',
+                    cls: 'search',
+                    items: [
+                        {
+                            xtype: 'rallytextfield',
+                            cls: 'search-text',
+                            itemId: 'searchText',
+                            enableKeyEvents: true,
+                            emptyText: 'Search',
+                            listeners: {
+                                specialkey: this._onSearchTextSpecialKey,
+                                scope: this
+                            }
+                        },
+                        {
+                            xtype: 'component',
+                            cls: 'search-button',
+                            listeners: {
+                                click: {
+                                    element: 'el',
+                                    fn: this._onSearchClicked,
+                                    scope: this
+                                }
+                            }
+                        }
+                    ]
+                }
+            );
+        },
+
+        initComponent: function() {
+            this.callParent(arguments);
+
+            this.on('afterrender', function() {
+                var cls = 'planning-column backlog';
+                this.getContentCell().addCls(cls);
+                this.getStatusCell().addCls(cls);
+                this.getColumnHeaderCell().addCls(cls);
+            }, this, {single: true});
+        },
+
+        _onSearchClicked: function() {
+            this._refreshColumn();
+        },
+
+        _onSearchTextSpecialKey: function(searchTextField, e) {
+            if (e.getKey() === e.ENTER) {
+                this._refreshColumn();
+            }
+        },
+
+        _refreshColumn: function() {
+            if (this.searching) {
+                return;
+            }
+
+            this.searching = true;
+            var searchValue = this.getColumnHeader().down('#searchText').getValue();
+            this.setMaskTarget(this.getContentCell());
+            this.showMask();
+            this._deactivatedCards = [];
+
+            this.on('load', function() {
+                this.fireEvent('filter', this);
+                this.hideMask();
+                this.searching = false;
+            }, this, {single: true});
+
+            this.refresh({
+                storeConfig: {
+                    search: searchValue ? Ext.String.trim(searchValue) : ""
+                }
+            });
+        },
+
+        getStoreFilter: function(model) {
+            var filters = [];
+            Ext.Array.push(filters, this.callParent(arguments));
+            if (model.elementName.indexOf('PortfolioItem') !== -1) {
+                  filters.push({
+                      property: 'Release',
+                      value: null
+                  });
+            }
+
+            return filters;
+        },
+
+        isMatchingRecord: function(record) {
+            var isMatching = this.callParent(arguments);
+            if (record.self.elementName.indexOf('PortfolioItem') !== -1) {
+                isMatching = isMatching && (!record.hasField('Release') || record.get('Release') === null);
+            }
+            return isMatching;
+        }
+    });
+})();
---
+++ ReleasePlanningBoardColumnProgressBar.js
@@ -0,0 +1,50 @@
+(function() {
+    var Ext = window.Ext4 || window.Ext;
+
+    /**
+     * The column Status for planning board columns on a cardboard.
+     */
+
+    Ext.define('Rally.apps.releaseplanningboard.ReleasePlanningBoardColumnProgressBar', {
+        extend: 'Ext.Component',
+        alias: 'widget.rallyreleaseplanningboardcolumnprogressbar',
+
+        renderTpl: Ext.create('Rally.ui.renderer.template.progressbar.TimeboxProgressBarTemplate', {
+            height: '14px',
+            width: '80%'
+        }),
+
+        update: function() {
+            var html = this.renderTpl.apply(this._getRenderData());
+            this.callParent([html]);
+        },
+
+        _getColumn: function() {
+            return this.column;
+        },
+
+        _getRenderData: function() {
+            var totalPointCount = this._getTotalPointCount();
+            var plannedVelocity = this._getPlannedVelocity();
+            return {
+                percentDone: totalPointCount / plannedVelocity,
+                amountComplete: totalPointCount,
+                total: plannedVelocity
+            };
+        },
+
+        _getTotalPointCount: function() {
+            return _.reduce(this._getColumn().getCards(true), function(memo, card) {
+                var planEstimate = card.getRecord().get('PreliminaryEstimate') ? parseInt(card.getRecord().get('PreliminaryEstimate').Value, 10) : 0;
+                return Ext.isNumber(planEstimate) ? memo + planEstimate : memo;
+            }, 0);
+        },
+
+        _getPlannedVelocity: function() {
+            return _.reduce(this._getColumn().getTimeboxRecords(), function(memo, record) {
+                var plannedVelocity = record.get('PlannedVelocity');
+                return Ext.isNumber(plannedVelocity) ? memo + plannedVelocity : memo;
+            }, 0);
+        }
+    });
+})();
